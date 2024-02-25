fd9e37b487378ad529be4a286254d10990c8692a
vismelikek <155015149+vismelikek@users.noreply.github.com>
2024-02-25 17:30:11 +0300

    Update airspeed.h

--- airspeed.h
+++ airspeed.h
@@ -1,1 +1,20 @@
+#ifndef AIRSPEED_H
+#define AIRSPEED_H
+
+#include <Wire.h>  
+#include <Arduino.h>
+
+#endif
+
+class AIRSPEED {
+  public:
+    AIRSPEED();
+    byte fetchPressure(unsigned int *p_P_dat);
+    float AS();
+
+  private:
+    byte address;
+    float initial_PR;
+    float k;
+};
 
